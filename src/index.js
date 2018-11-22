22c4f016bb2988bf812942ba906f4daa9e9c431f
scps940327 <scps940327@gamil.com>
2018-11-22 18:07:23 +0800

    20181122初始化程成webpack+react的檔案架構
    初始化程成webpack+react的檔案架構

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import FormContainer from "./js/components/container/FormContainer";
+// import style from './scss/style.scss';
+
+require('./scss/style.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,48 @@
-var path = require('path')
-var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin'); // 使用 extract text webpack plugin
-//var webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-var config = {
-    //target:'electron',
-    entry: ['./main.js'], // 在 index 檔案後的 .js 副檔名是可選的
-    output: {
-        path: path.resolve(__dirname, 'dist/js'),
-        publicPath: path.resolve(__dirname, 'dist/'),
-        filename: 'bundle.js'
-    },
-    //watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use:ExtractTextPlugin.extract({
-                    fallback:"style-loader",
-                    use:"css-loader"
-                })
-            },
-            {
-                test: /\.scss$/,
-                use:ExtractTextPlugin.extract({
-                    fallback:"style-loader",
-                    use:[{
-                        loader:"css-loader"
-                    },{
-                        loader:"sass-loader"
-                    }]
-                })
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {limit: 1000000, outputPath: 'img/'},
-                    
-                    },
-                    'image-webpack-loader'
-                ]
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '../css/fonts/'
-                    }
-                }]
-            }
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin('../css/bundle.css'),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000
+            }
+          }
+        ]
+      }
     ]
-}
-
-//config.target = webpackTargetElectronRenderer(config);
-module.exports = config;
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "../index.html"
+    })
+  ]
+};
